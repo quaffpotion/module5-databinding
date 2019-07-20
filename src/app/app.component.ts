fbff99e8b6c89264ebb77469683bfb7293aee672
quaffpotion <43005901+quaffpotion@users.noreply.github.com>
2019-07-20 13:15:17 -0500

    5-67: @Output, Custom Events, child to parent communication, note the $event

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,22 @@ export class AppComponent {
   serverElements = [
     { type: 'server', name: 'testserver', content: 'just a test' },
     { type: 'blueprint', name: 'testserver', content: 'just a test' },
-    { type: 'server', name: 'testserver', content: 'just a test' },
   ];
+
+  onServerAdded(serverData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
+
+  onBlueprintAdded(blueprintData: {blueprintName: string, blueprintContent: string}) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: blueprintData.blueprintName,
+      content: blueprintData.blueprintContent
+    });
+  }
   
 }
