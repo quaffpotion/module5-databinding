83df42f193a964f50eb4ba6f4367197218c82d9d
quaffpotion <43005901+quaffpotion@users.noreply.github.com>
2019-07-22 17:44:36 -0500

    @ ContentChild, *needs fix*

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, OnChanges, SimpleChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy, ContentChild, ElementRef, Query } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -13,11 +13,14 @@ export class ServerElementComponent implements
   AfterContentChecked,
   AfterViewInit,
   AfterViewChecked,
-  OnDestroy   {
+  OnDestroy,
+  ContentChild,
+  Query   {
 
   //6-66 assigns an alias; must bind to srvElement instead of element
   @Input('srvElement') element: {type: string, name: string, content: string}
   @Input() name: string
+  @ContentChild('contentParagraph') paragraph: ElementRef
 
   constructor() { 
     console.log('constructor called')
@@ -31,6 +34,7 @@ export class ServerElementComponent implements
 
   ngOnInit() {
     console.log('ngOninit called')
+    console.log('paragraph text: ' + this.paragraph.nativeElement.textContent) //will fail
   }
 
   ngDoCheck() {
@@ -39,6 +43,7 @@ export class ServerElementComponent implements
 
   ngAfterContentInit() {
     console.log('ngAfterContentInit')
+    console.log('paragraph text: ' + this.paragraph.nativeElement.textContent)
   }
 
   ngAfterContentChecked() {
